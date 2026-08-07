52e858849f1d0edcbce7d41f48a9ec69235d9265
Vanessacsyyyy <126734572+Vanessacsyyyy@users.noreply.github.com>
2026-08-07 09:39:36 +0800

    Refresh offline lesson cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='nihon-loop-v4';
-const CORE=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='nihon-loop-v5';
+const CORE=['./','./index.html','./styles.css','./lesson-data.js','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{if(e.request.url.includes('/public/photos/')){const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));}return res;}))));
